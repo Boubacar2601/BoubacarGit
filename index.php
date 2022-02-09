4d825e16c6b1236c9f292e6676d72371323055f8
Boubacar Dieng <boubacardieng85@gmail.com>
2022-02-09 11:41:01 +0100

    dev caroussel + main index + footer

--- index.php
+++ index.php
@@ -37,9 +37,51 @@
       </div>
     </div>
   </nav>
+
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imgs/paysage1.jpeg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imgs/paysage2.jpeg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imgs/paysage3.jpeg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
+
 </header>
 
+<main> 
     <h1 class="display-4 text-center my-5">Boubacar GIT</h1>
+
+    <div class="container">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores possimus accusantium quia aliquam nulla provident quibusdam optio molestias? Omnis necessitatibus officiis harum sapiente beatae aliquid ullam qui. Distinctio repudiandae explicabo enim quod, nesciunt velit. Laboriosam quasi quisquam similique quaerat quia laborum, quod pariatur atque, odio optio neque quam provident, sed assumenda! Distinctio, dolores iusto. Repudiandae quod, reprehenderit exercitationem impedit beatae neque perferendis corrupti omnis quaerat voluptatibus culpa, labore dignissimos error consequatur et possimus. Quae temporibus eaque voluptate nisi atque earum quidem eligendi, adipisci quibusdam quod fugit corporis repudiandae blanditiis quasi voluptatum. Provident modi laboriosam ratione, asperiores voluptatem exercitationem ullam, minima magnam enim libero minus voluptatibus adipisci deserunt aliquid, temporibus laudantium nihil. Perferendis voluptatum, et dicta distinctio quas voluptates consequatur veniam explicabo nemo quis cupiditate quaerat similique. Officiis magnam alias eos possimus dolor dolorum! Magni incidunt in, commodi harum aliquam veritatis eum sunt facilis ad voluptatum nemo debitis ipsam minus possimus porro unde rem ut necessitatibus eligendi. Dignissimos architecto quas explicabo molestiae voluptates, magnam delectus dicta animi laborum saepe voluptatem unde hic perspiciatis vero ducimus. Facere sequi a accusantium recusandae eum minima aliquam iure ad repudiandae nemo velit similique neque consectetur ducimus quam asperiores delectus vitae qui doloribus explicabo, at rerum quos vero quaerat? Reiciendis assumenda impedit, ipsam soluta ea est. Provident velit rem modi quibusdam qui eos deserunt consequuntur, autem sed, ratione dolorum dicta porro nisi at totam maxime? Odit nam, molestias, dolorem quasi delectus ducimus quisquam, eius quam vitae nesciunt unde quis cum est doloremque assumenda culpa rem distinctio voluptates eligendi ex? Recusandae quibusdam at ipsa impedit adipisci quasi fugit quae voluptas! Enim ab dolore soluta voluptates voluptatem natus aut at id nulla, quod odit possimus in asperiores sapiente maiores rerum facilis quibusdam, officia quisquam. Illo, fugit earum quia beatae maxime reprehenderit qui minima quis. Debitis dicta ipsa nesciunt eveniet molestias minima et, doloribus, expedita porro suscipit sed alias eligendi assumenda amet est placeat quae cupiditate libero? Quos aspernatur laborum dolore tempora quisquam numquam in odio consequatur neque, inventore voluptatem adipisci ab repellat nam blanditiis officiis aperiam delectus. Saepe inventore excepturi sequi quasi ab fuga fugiat, debitis ducimus hic distinctio harum voluptate amet, dicta doloribus optio suscipit recusandae, facere cumque dolorem nobis unde. Ad nobis amet in, illum voluptatibus explicabo, itaque iste molestiae aliquid assumenda dicta veritatis</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores possimus accusantium quia aliquam nulla provident quibusdam optio molestias? Omnis necessitatibus officiis harum sapiente beatae aliquid ullam qui. Distinctio repudiandae explicabo enim quod, nesciunt velit. Laboriosam quasi quisquam similique quaerat quia laborum, quod pariatur atque, odio optio neque quam provident, sed assumenda! Distinctio, dolores iusto. Repudiandae quod, reprehenderit exercitationem impedit beatae neque perferendis corrupti omnis quaerat voluptatibus culpa, labore dignissimos error consequatur et possimus. Quae temporibus eaque voluptate nisi atque earum quidem eligendi, adipisci quibusdam quod fugit corporis repudiandae blanditiis quasi voluptatum. Provident modi laboriosam ratione, asperiores voluptatem exercitationem ullam, minima magnam enim libero minus voluptatibus adipisci deserunt aliquid, temporibus laudantium nihil. Perferendis voluptatum, et dicta distinctio quas voluptates consequatur veniam explicabo nemo quis cupiditate quaerat similique. Officiis magnam alias eos possimus dolor dolorum! Magni incidunt in, commodi harum aliquam veritatis eum sunt facilis ad voluptatum nemo debitis ipsam minus possimus porro unde rem ut necessitatibus eligendi. Dignissimos architecto quas explicabo molestiae voluptates, magnam delectus dicta animi laborum saepe voluptatem unde hic perspiciatis vero ducimus. Facere sequi a accusantium recusandae eum minima aliquam iure ad repudiandae nemo velit similique neque consectetur ducimus quam asperiores delectus vitae qui doloribus explicabo, at rerum quos vero quaerat? Reiciendis assumenda impedit, ipsam soluta ea est. Provident velit rem modi quibusdam qui eos deserunt consequuntur, autem sed, ratione dolorum dicta porro nisi at totam maxime? Odit nam, molestias, dolorem quasi delectus ducimus quisquam, eius quam vitae nesciunt unde quis cum est doloremque assumenda culpa rem distinctio voluptates eligendi ex? Recusandae quibusdam at ipsa impedit adipisci quasi fugit quae voluptas! Enim ab dolore soluta voluptates voluptatem natus aut at id nulla, quod odit possimus in asperiores sapiente maiores rerum facilis quibusdam, officia quisquam. Illo, fugit earum quia beatae maxime reprehenderit qui minima quis. Debitis dicta ipsa nesciunt eveniet molestias minima et, doloribus, expedita porro suscipit sed alias eligendi assumenda amet est placeat quae cupiditate libero? Quos aspernatur laborum dolore tempora quisquam numquam in odio consequatur neque, inventore voluptatem adipisci ab repellat nam blanditiis officiis aperiam delectus. Saepe inventore excepturi sequi quasi ab fuga fugiat, debitis ducimus hic distinctio harum voluptate amet, dicta doloribus optio suscipit recusandae, facere cumque dolorem nobis unde. Ad nobis amet in, illum voluptatibus explicabo, itaque iste molestiae aliquid assumenda dicta veritatis</p>
+
+
+    </div>
+    </main>
+
+<footer class="bg-dark text-center text-white py-3">
+    &copy; 2022 - GL - MonProjetGit - tous droits reservés
+</footer>
+
+
 
     <!-- Optional JavaScript; choose one of the two! -->
 
